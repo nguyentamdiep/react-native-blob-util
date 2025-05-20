dc02843a1306300891283101a4a80d9e32fa23d3
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-05-20 15:17:40 +0530

    try adding apis

--- windows/ReactNativeBlobUtil/ReactNativeBlobUtil.cpp
+++ windows/ReactNativeBlobUtil/ReactNativeBlobUtil.cpp
@@ -11,8 +11,82 @@ void ReactNativeBlobUtil::Initialize(React::ReactContext const &reactContext) no
   m_context = reactContext;
 }
 
-double ReactNativeBlobUtil::multiply(double a, double b) noexcept {
-  return a * b;
+// Constants method
+ReactNativeBlobUtilCodegen::Constants ReactNativeBlobUtil::getConstants() noexcept {
+    return {
+      .CacheDir = L"",
+      .DocumentDir = L"",
+      .DownloadDir = L"",
+      .LibraryDir = L"",
+      .MainBundleDir = L"",
+      .MovieDir = L"",
+      .MusicDir = L"",
+      .PictureDir = L"",
+      .ApplicationSupportDir = L"",
+      .RingtoneDir = L"",
+      .SDCardDir = L"",
+      .SDCardApplicationDir = L"",
+      .DCIMDir = L"",
+      .LegacyDCIMDir = L"",
+      .LegacyPictureDir = L"",
+      .LegacyMusicDir = L"",
+      .LegacyDownloadDir = L"",
+      .LegacyMovieDir = L"",
+      .LegacyRingtoneDir = L"",
+      .LegacySDCardDir = L""
+    };
 }
+
+// Stub implementations for all methods
+
+void ReactNativeBlobUtil::fetchBlobForm(JSValue const&, std::string const&, std::string const&, std::string const&, JSValue const&, JSValue const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::fetchBlob(JSValue const&, std::string const&, std::string const&, std::string const&, JSValue const&, std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+fire_and_forget ReactNativeBlobUtil::createFile(std::string const&, std::string const&, std::string const&) noexcept { co_return; }
+fire_and_forget ReactNativeBlobUtil::createFileASCII(std::string const&, JSValue const&) noexcept { co_return; }
+fire_and_forget ReactNativeBlobUtil::pathForAppGroup(std::string const&, React::ReactPromise<std::string> const& result) noexcept { result.Reject("Not implemented"); co_return; }
+std::string ReactNativeBlobUtil::syncPathAppGroup(std::string const&) noexcept { return ""; }
+void ReactNativeBlobUtil::exists(std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+fire_and_forget ReactNativeBlobUtil::writeFile(std::string const&, std::string const&, std::string const&, bool, bool, React::ReactPromise<int32_t> const& result) noexcept { result.Resolve(0); co_return; }
+fire_and_forget ReactNativeBlobUtil::writeFileArray(std::string const&, JSValue const&, bool, React::ReactPromise<int32_t> const& result) noexcept { result.Resolve(0); co_return; }
+void ReactNativeBlobUtil::writeStream(std::string const&, std::string const&, bool, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::writeArrayChunk(std::string const&, JSValue const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::writeChunk(std::string const&, std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::closeStream(std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::unlink(std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::removeSession(JSValue const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+fire_and_forget ReactNativeBlobUtil::ls(std::string const&, React::ReactPromise<JSValue> const& result) noexcept { result.Resolve(JSValueArray{}); co_return; }
+void ReactNativeBlobUtil::stat(std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::lstat(std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::cp(std::string const&, std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::mv(std::string const&, std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+fire_and_forget ReactNativeBlobUtil::mkdir(std::string const&, React::ReactPromise<bool> const& result) noexcept { result.Resolve(true); co_return; }
+fire_and_forget ReactNativeBlobUtil::readFile(std::string const&, std::string const&, bool, React::ReactPromise<JSValue> const& result) noexcept { result.Resolve(JSValueArray{}); co_return; }
+fire_and_forget ReactNativeBlobUtil::hash(std::string const&, std::string const&, React::ReactPromise<std::string> const& result) noexcept { result.Resolve(""); co_return; }
+void ReactNativeBlobUtil::readStream(std::string const&, std::string const&, int32_t, int32_t, std::string const&) noexcept {}
+void ReactNativeBlobUtil::getEnvironmentDirs(React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::cancelRequest(std::string const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::enableProgressReport(std::string const&, int32_t, int32_t) noexcept {}
+void ReactNativeBlobUtil::enableUploadProgressReport(std::string const&, int32_t, int32_t) noexcept {}
+fire_and_forget ReactNativeBlobUtil::slice(std::string const&, std::string const&, int64_t, int64_t, React::ReactPromise<std::string> const& result) noexcept { result.Resolve(""); co_return; }
+fire_and_forget ReactNativeBlobUtil::presentOptionsMenu(std::string const&, std::string const&, React::ReactPromise<JSValue> const& result) noexcept { result.Resolve(JSValueArray{}); co_return; }
+fire_and_forget ReactNativeBlobUtil::presentOpenInMenu(std::string const&, std::string const&, React::ReactPromise<JSValue> const& result) noexcept { result.Resolve(JSValueArray{}); co_return; }
+fire_and_forget ReactNativeBlobUtil::presentPreview(std::string const&, std::string const&, React::ReactPromise<JSValue> const& result) noexcept { result.Resolve(JSValueArray{}); co_return; }
+fire_and_forget ReactNativeBlobUtil::excludeFromBackupKey(std::string const&, React::ReactPromise<JSValue> const& result) noexcept { result.Resolve(JSValueArray{}); co_return; }
+void ReactNativeBlobUtil::df(React::ReactCallback<const JSValue&> const&) noexcept {}
+void ReactNativeBlobUtil::emitExpiredEvent(React::ReactCallback<std::string const&> const&) noexcept {}
+fire_and_forget ReactNativeBlobUtil::actionViewIntent(std::string const&, std::string const&, std::string const&) noexcept { co_return; }
+fire_and_forget ReactNativeBlobUtil::addCompleteDownload(JSValue const&) noexcept { co_return; }
+fire_and_forget ReactNativeBlobUtil::copyToInternal(std::string const&, std::string const&, React::ReactPromise<std::string> const& result) noexcept { result.Resolve(""); co_return; }
+fire_and_forget ReactNativeBlobUtil::copyToMediaStore(JSValue const&, std::string const&, std::string const&, React::ReactPromise<std::string> const& result) noexcept { result.Resolve(""); co_return; }
+fire_and_forget ReactNativeBlobUtil::createMediaFile(JSValue const&, std::string const&, React::ReactPromise<std::string> const& result) noexcept { result.Resolve(""); co_return; }
+fire_and_forget ReactNativeBlobUtil::getBlob(std::string const&, std::string const&, React::ReactPromise<JSValue> const& result) noexcept { result.Resolve(JSValueArray{}); co_return; }
+fire_and_forget ReactNativeBlobUtil::getContentIntent(std::string const&, React::ReactPromise<std::string> const& result) noexcept { result.Resolve(""); co_return; }
+fire_and_forget ReactNativeBlobUtil::getSDCardDir(React::ReactPromise<std::string> const& result) noexcept { result.Resolve(""); co_return; }
+fire_and_forget ReactNativeBlobUtil::getSDCardApplicationDir(React::ReactPromise<std::string> const& result) noexcept { result.Resolve(""); co_return; }
+void ReactNativeBlobUtil::scanFile(JSValue const&, React::ReactCallback<const JSValue&> const&) noexcept {}
+fire_and_forget ReactNativeBlobUtil::writeToMediaFile(std::string const&, std::string const&, bool, React::ReactPromise<std::string> const& result) noexcept { result.Resolve(""); co_return; }
+
+void ReactNativeBlobUtil::addListener(std::string const&) noexcept {}
+void ReactNativeBlobUtil::removeListeners(int32_t) noexcept {}
 
 } // namespace winrt::ReactNativeBlobUtil
--- windows/ReactNativeBlobUtil/ReactNativeBlobUtil.h
+++ windows/ReactNativeBlobUtil/ReactNativeBlobUtil.h
@@ -3,11 +3,11 @@
 #include "pch.h"
 #include "resource.h"
 
-#if __has_include("codegen/NativeReactNativeBlobUtilDataTypes.g.h")
-  #include "codegen/NativeReactNativeBlobUtilDataTypes.g.h"
+#if __has_include("codegen/NativeBlobUtilsDataTypes.g.h")
+  #include "codegen/NativeBlobUtilsDataTypes.g.h"
 #endif
-#include "codegen/NativeReactNativeBlobUtilSpec.g.h"
 
+#include "codegen/NativeBlobUtilsSpec.g.h"
 #include "NativeModules.h"
 
 namespace winrt::ReactNativeBlobUtil
@@ -16,13 +16,157 @@ namespace winrt::ReactNativeBlobUtil
 REACT_MODULE(ReactNativeBlobUtil)
 struct ReactNativeBlobUtil
 {
-  using ModuleSpec = ReactNativeBlobUtilCodegen::ReactNativeBlobUtilSpec;
+  using ModuleSpec = ReactNativeBlobUtilCodegen::BlobUtilsSpec;
 
   REACT_INIT(Initialize)
-  void Initialize(React::ReactContext const &reactContext) noexcept;
+  void Initialize(React::ReactContext const& reactContext) noexcept;
 
-  REACT_SYNC_METHOD(multiply)
-  double multiply(double a, double b) noexcept;
+  REACT_METHOD(getConstants)
+  ReactNativeBlobUtilCodegen::BlobUtilsSpec_Constants* getConstants();
+
+  REACT_METHOD(fetchBlobForm)
+  void fetchBlobForm(JSValue options, std::string const& taskId, std::string const& method, std::string const& url, JSValue headers, JSValue form, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(fetchBlob)
+  void fetchBlob(JSValue options, std::string const& taskId, std::string const& method, std::string const& url, JSValue headers, std::string const& body, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(createFile)
+  winrt::Windows::Foundation::IAsyncAction createFile(std::string const& path, std::string const& data, std::string const& encoding);
+
+  REACT_METHOD(createFileASCII)
+  winrt::Windows::Foundation::IAsyncAction createFileASCII(std::string const& path, JSValue data);
+
+  REACT_METHOD(pathForAppGroup)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> pathForAppGroup(std::string const& groupName);
+
+  REACT_METHOD(syncPathAppGroup)
+  std::string syncPathAppGroup(std::string const& groupName);
+
+  REACT_METHOD(exists)
+  void exists(std::string const& path, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(writeFile)
+  winrt::Windows::Foundation::IAsyncOperation<int> writeFile(std::string const& path, std::string const& encoding, std::string const& data, bool transformFile, bool append);
+
+  REACT_METHOD(writeFileArray)
+  winrt::Windows::Foundation::IAsyncOperation<int> writeFileArray(std::string const& path, JSValue data, bool append);
+
+  REACT_METHOD(writeStream)
+  void writeStream(std::string const& path, std::string const& withEncoding, bool appendData, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(writeArrayChunk)
+  void writeArrayChunk(std::string const& streamId, JSValue withArray, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(writeChunk)
+  void writeChunk(std::string const& streamId, std::string const& withData, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(closeStream)
+  void closeStream(std::string const& streamId, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(unlink)
+  void unlink(std::string const& path, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(removeSession)
+  void removeSession(JSValue paths, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(ls)
+  winrt::Windows::Foundation::IAsyncOperation<JSValue> ls(std::string const& path);
+
+  REACT_METHOD(stat)
+  void stat(std::string const& target, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(lstat)
+  void lstat(std::string const& path, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(cp)
+  void cp(std::string const& src, std::string const& dest, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(mv)
+  void mv(std::string const& path, std::string const& dest, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(mkdir)
+  winrt::Windows::Foundation::IAsyncOperation<bool> mkdir(std::string const& path);
+
+  REACT_METHOD(readFile)
+  winrt::Windows::Foundation::IAsyncOperation<JSValue> readFile(std::string const& path, std::string const& encoding, bool transformFile);
+
+  REACT_METHOD(hash)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> hash(std::string const& path, std::string const& algorithm);
+
+  REACT_METHOD(readStream)
+  void readStream(std::string const& path, std::string const& encoding, int bufferSize, int tick, std::string const& streamId);
+
+  REACT_METHOD(getEnvironmentDirs)
+  void getEnvironmentDirs(std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(cancelRequest)
+  void cancelRequest(std::string const& taskId, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(enableProgressReport)
+  void enableProgressReport(std::string const& taskId, int interval, int count);
+
+  REACT_METHOD(enableUploadProgressReport)
+  void enableUploadProgressReport(std::string const& taskId, int interval, int count);
+
+  REACT_METHOD(slice)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> slice(std::string const& src, std::string const& dest, int64_t start, int64_t end);
+
+  REACT_METHOD(presentOptionsMenu)
+  winrt::Windows::Foundation::IAsyncOperation<JSValue> presentOptionsMenu(std::string const& uri, std::string const& scheme);
+
+  REACT_METHOD(presentOpenInMenu)
+  winrt::Windows::Foundation::IAsyncOperation<JSValue> presentOpenInMenu(std::string const& uri, std::string const& scheme);
+
+  REACT_METHOD(presentPreview)
+  winrt::Windows::Foundation::IAsyncOperation<JSValue> presentPreview(std::string const& uri, std::string const& scheme);
+
+  REACT_METHOD(excludeFromBackupKey)
+  winrt::Windows::Foundation::IAsyncOperation<JSValue> excludeFromBackupKey(std::string const& url);
+
+  REACT_METHOD(df)
+  void df(std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(emitExpiredEvent)
+  void emitExpiredEvent(std::function<void(std::string)> const& callback);
+
+  REACT_METHOD(actionViewIntent)
+  winrt::Windows::Foundation::IAsyncAction actionViewIntent(std::string const& path, std::string const& mime, std::string const& chooserTitle);
+
+  REACT_METHOD(addCompleteDownload)
+  winrt::Windows::Foundation::IAsyncAction addCompleteDownload(JSValue config);
+
+  REACT_METHOD(copyToInternal)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> copyToInternal(std::string const& contentUri, std::string const& destPath);
+
+  REACT_METHOD(copyToMediaStore)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> copyToMediaStore(JSValue filedata, std::string const& mt, std::string const& path);
+
+  REACT_METHOD(createMediaFile)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> createMediaFile(JSValue filedata, std::string const& mt);
+
+  REACT_METHOD(getBlob)
+  winrt::Windows::Foundation::IAsyncOperation<JSValue> getBlob(std::string const& contentUri, std::string const& encoding);
+
+  REACT_METHOD(getContentIntent)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> getContentIntent(std::string const& mime);
+
+  REACT_METHOD(getSDCardDir)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> getSDCardDir();
+
+  REACT_METHOD(getSDCardApplicationDir)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> getSDCardApplicationDir();
+
+  REACT_METHOD(scanFile)
+  void scanFile(JSValue pairs, std::function<void(JSValue)> const& callback);
+
+  REACT_METHOD(writeToMediaFile)
+  winrt::Windows::Foundation::IAsyncOperation<std::string> writeToMediaFile(std::string const& fileUri, std::string const& path, bool transformFile);
+
+  REACT_METHOD(addListener)
+  void addListener(std::string const& eventName);
+
+  REACT_METHOD(removeListeners)
+  void removeListeners(int count);
 
 private:
   React::ReactContext m_context;
