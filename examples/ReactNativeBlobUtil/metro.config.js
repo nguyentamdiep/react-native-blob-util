3869c2cb3c771675aab6c39ca89561c8219244cc
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-05-21 22:24:12 +0530

    Try adding examples changes

--- examples/ReactNativeBlobUtil/metro.config.js
+++ examples/ReactNativeBlobUtil/metro.config.js
@@ -1,27 +1,51 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = {
-    resolver: {
-        blacklistRE: blacklist([
-            // This stops "react-native run-windows" from causing the metro server to crash if its already running
-            new RegExp(`${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`),
-            // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
-            /.*\.ProjectImports\.zip/,
-        ]),
+const fs = require('fs');
+const path = require('path');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const rnwPath = fs.realpathSync(
+  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
+);
+
+//
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
+const config = {
+  //
+  resolver: {
+    extraNodeModules: {
+      "react-native-blob-util": path.resolve(
+        __dirname,
+        "../../"
+      ),
     },
-    transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: false,
-            },
-        }),
-    },
+    blockList: exclusionList([
+      // This stops "npx @react-native-community/cli run-windows" from causing the metro server to crash if its already running
+      new RegExp(
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+      ),
+      // This prevents "npx @react-native-community/cli run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
+      new RegExp(`${rnwPath}/build/.*`),
+      new RegExp(`${rnwPath}/target/.*`),
+      /.*\.ProjectImports\.zip/,
+    ]),
+    //
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
